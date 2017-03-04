733e5f2d140b9915937c665f6933006c7bb5cb29
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2017-03-04 14:59:56 -0300

    Add Angular 2 Course

---
+++ Angular 2 - The Complete Guide/first-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DatabindingComponent } from './databinding/databinding.component';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Root Component</h1>
+    <fa-databinding></fa-databinding>
+  `,
+})
+export class AppComponent {
+  title = 'app works!';
+}
