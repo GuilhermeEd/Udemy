65ad83f5e811dd182ae3d8236a96402de5fec0c1
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2017-03-04 16:28:50 -0300

    Event Binding Class

--- Angular 2 - The Complete Guide/first-app/src/app/databinding/databinding.component.ts
+++ Angular 2 - The Complete Guide/first-app/src/app/databinding/databinding.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'fa-databinding',
   templateUrl: './databinding.component.html',
   styleUrls: ['./databinding.component.css']
 })
-export class DatabindingComponent implements OnInit {
+export class DatabindingComponent {
     stringInterpolation = 'This is a string interpolation!';
     numberInterpolation = 2;
-    ngOnInit(){}
+
+    OnTest(){
+      return true;
+    }
+
+    onClicked(value: string){
+      alert(value);
+    }
 }
