e88d8a4d02b19f1146c4fa026008ffcd27b895a6
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2017-03-08 16:41:45 -0300

    HostListener and HostBinding Examples

--- Angular 2 - The Complete Guide/02-directives/src/app/highlight.directive.ts
+++ Angular 2 - The Complete Guide/02-directives/src/app/highlight.directive.ts
@@ -1,13 +1,21 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
 export class HighlightDirective {
-  // creates a private property and assign to it the value of the referenced element
-  constructor(private elementRef: ElementRef, private renderer: Renderer) {
-    // this.elementRef.nativeElement.style.backgroundColor = 'green';
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'backgroundColor', 'green');
+  @HostListener('mouseenter') mouseover(){
+      this.backgroundColor = 'green';
+  };
+  @HostListener('mouseleave') mouseleave(){
+      this.backgroundColor = 'white';
+  };
+  @HostBinding('style.backgroundColor') get setColor(){
+    return this.backgroundColor;
+  }
+  private backgroundColor = 'white';
+
+  constructor() {
   }
 
 }
