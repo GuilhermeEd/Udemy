c3ce54cd14d70afa317a856851ee1ce751ab6564
WillhelmEdward <guilherme.eduardo92@hotmail.com>
2017-05-07 13:50:18 -0300

    Added Quotes Project (First Ionic Project)

---
+++ Ionic 2 - The Practical Guide to Building iOS & Android Apps/quotes/src/pages/quotes/quotes.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+
+@IonicPage()
+@Component({
+  selector: 'page-quotes',
+  templateUrl: 'quotes.html',
+})
+export class QuotesPage implements OnInit {
+  quoteGroup: {category: string, quotes: Quote[], icon: string};
+
+  constructor(private navParams: NavParams){}
+
+  ngOnInit(){
+    this.quoteGroup = this.navParams.data;
+  }
+}
