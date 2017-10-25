09ffb3df20ed1b2358b0894594cfbd186e9464a2
WillhelmEdward <guilherme.eduardo92@hotmail.com>
2017-10-24 22:04:28 -0200

    Added VueJS Components Exercise

---
+++ VueJS 2 - The Complete Guide/Time to Practice - Components/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header/>
+        <hr>
+        <div class="row">
+            <app-servers/>
+            <app-server-details/>
+        </div>
+        <hr>
+        <app-footer/>
+    </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import ServerDetails from './components/ServerDetails.vue';
+import Servers from './components/Servers.vue';
+
+export default {
+    components: {
+        appHeader: Header,
+        appFooter: Footer,
+        appServerDetails: ServerDetails,
+        appServers: Servers
+        }
+}
+</script>
+
+<style>
+
+</style>
