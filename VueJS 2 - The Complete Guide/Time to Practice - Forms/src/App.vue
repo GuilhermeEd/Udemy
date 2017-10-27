67019be05c2f7c99435d872928d49d5aaaa6a3c8
-g <guilherme.eduardo92@hotmail.com>
2017-10-27 19:12:30 -0200

    Forms Exercise: Still in Progress

--- VueJS 2 - The Complete Guide/Time to Practice - Forms/src/App.vue
+++ VueJS 2 - The Complete Guide/Time to Practice - Forms/src/App.vue
@@ -5,23 +5,51 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
+                    <form v-if="!formWasSubmitted" style="padding-top: 15px;">
+                        <!-- Full Name (First Name + Last Name) -->
+                        <div class="form-group">
+                            <label for="firstName">First Name:</label>
+                            <input name="firstName" id="firstName" type="text" class="form-control">
+                            <label for="lastName">Last Name:</label>
+                            <input name="lastName" id="lastName" type="text" class="form-control">
+                        </div>
+                        <!-- Mail -->
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input name="email" id="email" type="email" class="form-control">
+                        </div>
+                        <!-- Password -->
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input name="password" id="password" type="password" class="form-control">
+                        </div>
+                        <!-- Store Data? Yes/No -->
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input type="radio" class="form-check-input">
+                                Yes
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <label class="form-check-label">
+                                <input type="radio" class="form-check-input">
+                                No
+                            </label>
+                        </div>
+                        <!-- Exercise 2 -->
+                        <!-- Only display the Form if it has NOT been submitted -->
+                        <button
+                            type="submit"
+                            class="btn btn-primary"
+                            @click.prevent="formWasSubmitted = true">
+                            Submit</button>
+                    </form>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+            <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+        <div v-if="formWasSubmitted" class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -37,10 +65,18 @@
             </div>
         </div>
     </div>
+    <!-- Exercise 3 -->
+    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+    <!-- which still holds the First Name and Last Name Input Field -->
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+                formWasSubmitted: false
+            }
+        }
     }
 </script>
 
