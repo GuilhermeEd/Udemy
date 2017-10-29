53fdbc585ac0a2a8f365d378603e8d9620f3ddfd
-g <guilherme.eduardo92@hotmail.com>
2017-10-29 16:50:21 -0200

    Directives Exercise Final

--- VueJS 2 - The Complete Guide/Time to Practice - Directives/src/App.vue
+++ VueJS 2 - The Complete Guide/Time to Practice - Directives/src/App.vue
@@ -5,15 +5,52 @@
                 <h1>Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
+                <div class="alert alert-info">
+                    <p><strong>Event:</strong> {{ event }}</p>
+                </div>
+                <button v-myOn:click="buttonClick" class="btn btn-primary">Click Me!</button>
+                <hr>
+                <div
+                    v-myOn:mouseenter="mouseEnter"
+                    v-myOn:mouseleave="mouseLeave"
+                    class="box"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data(){
+            return{
+                event: 'waiting for event...'
+            }
+        },
+        methods: {
+            buttonClick(){
+                this.showEvent('Button Click');
+            },
+            mouseEnter(){
+                this.showEvent('Mouse Enter');
+            },
+            mouseLeave(){
+                this.showEvent('Mouse Leave');
+            },
+            showEvent(e){
+                this.event = e;
+                let that = this;
+                setTimeout(function(){
+                    that.event = 'waiting for event...';
+                },1000);
+            }
+        }
+    }
 </script>
 
-<style>
+<style scoped>
+.box{
+    width: 100px;
+    height: 100px;
+    background-color: lightgreen
+}
 </style>
--- VueJS 2 - The Complete Guide/Time to Practice - Directives/src/main.js
+++ VueJS 2 - The Complete Guide/Time to Practice - Directives/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('myOn', {
+  bind(el, binding){
+    const eventType = binding.arg;
+    const callbackFunction = binding.value;
+    el.addEventListener(eventType, function(){ callbackFunction() });
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
