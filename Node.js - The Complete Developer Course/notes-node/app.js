f4185730001784b8670ee78fa837f57898ca6f96
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-02-19 00:18:17 -0300

    Using 3rd Party Modules

--- Node.js - The Complete Developer Course/notes-node/app.js
+++ Node.js - The Complete Developer Course/notes-node/app.js
@@ -2,12 +2,19 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
+console.log(_.isString(true));
+console.log(_.isString('Guilherme'));
+
+var filteredArray = _.uniq(['A', 'B', 'B', 'C', 'C', 'C']);
+console.log(filteredArray);
+
+/* var res = notes.addNote();
 console.log(res);
 
 console.log('Result: ', notes.add(9, -2));
 
-var user = os.userInfo();
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+ var user = os.userInfo();
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);  */
