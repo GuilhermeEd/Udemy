c392a238f43d13b4df5a831a1b0ceb1dd616a616
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-02-18 23:32:45 -0300

    Added Node.js Course

---
+++ Node.js - The Complete Developer Course/notes-node/app.js
@@ -0,0 +1,13 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+var res = notes.addNote();
+console.log(res);
+
+console.log('Result: ', notes.add(9, -2));
+
+var user = os.userInfo();
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
