2a4f8c4e33e0a7426d176dc49fce572b56615065
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2018-02-21 16:29:55 -0300

    Added Google Maps Geocoding API key

--- Node.js - The Complete Developer Course/weather-app/app.js
+++ Node.js - The Complete Developer Course/weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombardstreet%20philadelphia',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombardstreet%20philadelphia&key=AIzaSyACSDpFLqMUDPogWHYMayiOvWol6sQYXBs',
   json: true
 }, (error, response, body) => {
   console.log(body);
