bbcbbbea575eb1bf977c57a63031ec9aaf797b72
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-02-25 17:30:56 -0300

    Wiring Up Weather Search

--- Node.js - The Complete Developer Course/weather-app/app.js
+++ Node.js - The Complete Developer Course/weather-app/app.js
@@ -3,17 +3,17 @@ const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
 
 const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+})
+.help()
+.alias('help', 'h')
+.argv;
 
 geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
@@ -23,3 +23,18 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   }
 });
 
+
+const request = require('request');
+
+request({
+  url: 'https://api.darksky.net/forecast/ff04c1cba2cce928e86d531d31e332b1/37.8267,-122.4233',
+  json: true
+}, (error, response, body) => {
+  if (error) {
+    console.log('Unable to connect to Forecast.io server.');
+  } else if (response.statusCode === 400) {
+    console.log('Unable to fetch weather.');
+  } else if (response.statusCode === 200) {
+    console.log(body.currently.temperature);
+  }
+})
