a4d5e20df26a300981038bf4182fc52fae1e2cc6
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-03-01 23:02:17 -0300

    Testing Async Code

--- Node.js - The Complete Developer Course/node-tests/utils/utils.test.js
+++ Node.js - The Complete Developer Course/node-tests/utils/utils.test.js
@@ -7,10 +7,24 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
 it('should square a number', () => {
   var res = utils.square(3);
   expect(res).toBe(9).toBeA('number');
 });
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(5, (square) => {
+    expect(square).toBe(25).toBeA('number');
+    done();
+  });
+})
 
 it('should set first and last name', () => {
   var user = { location: 'Brazil', age: 25 };
