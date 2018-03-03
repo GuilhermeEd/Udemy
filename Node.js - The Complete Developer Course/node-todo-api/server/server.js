3fc56d2e0badbd9492cae00f6a2abb40431db573
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-03-03 10:36:28 -0300

    Validators, Types and Defaults

--- Node.js - The Complete Developer Course/node-todo-api/server/server.js
+++ Node.js - The Complete Developer Course/node-todo-api/server/server.js
@@ -5,13 +5,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo =  mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false,
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null,
   }
 });
 
@@ -25,7 +30,7 @@ newTodo.save().then((doc) => {
   console.log('Unable to save todo');
 }); */
 
-var newTodo = new Todo({
+/* var newTodo = new Todo({
   text: 'Lunch',
   completed: true,
   completedAt: 1520035200
@@ -33,4 +38,23 @@ var newTodo = new Todo({
   console.log('Saved todo', doc);
 }, (err) => {
   console.log('Unable to save todo');
+}); */
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+  }
+})
+
+var user = new User({
+  email: 'email@email.com',
+});
+
+user.save().then((doc) => {
+  console.log('User saved', doc);
+}, (err) => {
+  console.log('Unable to save user', err);
 });
