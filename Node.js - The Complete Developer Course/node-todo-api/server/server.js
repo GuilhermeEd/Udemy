9b7eb4f5525c672e09288b65929d4e3b204e5a2a
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-03-03 10:27:51 -0300

    Setting Up Mongoose

---
+++ Node.js - The Complete Developer Course/node-todo-api/server/server.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo =  mongoose.model('Todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+/* var newTodo = new Todo({
+  text: 'Cook dinner'
+});
+
+newTodo.save().then((doc) => {
+  console.log('Saved todo', doc);
+}, (err) => {
+  console.log('Unable to save todo');
+}); */
+
+var newTodo = new Todo({
+  text: 'Lunch',
+  completed: true,
+  completedAt: 1520035200
+}).save().then((doc) => {
+  console.log('Saved todo', doc);
+}, (err) => {
+  console.log('Unable to save todo');
+});
