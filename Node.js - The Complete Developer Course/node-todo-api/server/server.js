d3e571ea0ba8c98267793c98cd9fbf7a5aba27b9
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-03-09 00:41:54 -0300

    Creating a Test Database

--- Node.js - The Complete Developer Course/node-todo-api/server/server.js
+++ Node.js - The Complete Developer Course/node-todo-api/server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +10,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -93,7 +95,7 @@ app.patch('/todos/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Started up at port ${port}`);
+  console.log(`Started up at port ${port}.`);
 });
 
 module.exports = { app };
