0b98b06184672aa27c0ebe718d609707e585c529
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-03-18 16:34:45 -0300

    Hashing Passwords

--- Node.js - The Complete Developer Course/node-todo-api/playground/hashing.js
+++ Node.js - The Complete Developer Course/node-todo-api/playground/hashing.js
@@ -1,7 +1,22 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = '123abc';
+
+/* bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+}); */
+
+var hashedPassword = '$2a$10$MIs5MT8fGaHIshwodDH5XuRJnk0ogrcX3YL3xzdq95J4CRmV6R0Cm';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+/* var data = {
   id: 10
 };
 
@@ -9,7 +24,7 @@ var token = jwt.sign(data, 'secret');
 console.log(token);
 
 var decoded = jwt.verify(token, 'secret');
-console.log('decoded', decoded);
+console.log('decoded', decoded); */
 
 /* var message = 'I am user number 3';
 var hash = SHA256(message).toString();
