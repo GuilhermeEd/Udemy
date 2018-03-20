b0c01eab9a901d21bda3147e2b288bc61d1e4dd4
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-03-20 16:19:48 -0300

    Loggin In - POST /users/login

--- Node.js - The Complete Developer Course/node-todo-api/server/models/user.js
+++ Node.js - The Complete Developer Course/node-todo-api/server/models/user.js
@@ -69,6 +69,26 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((res, rej) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          res(user);
+        } else {
+          rej(err);
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
