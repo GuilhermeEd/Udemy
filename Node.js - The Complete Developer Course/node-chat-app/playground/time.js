b57df9b9d121ca1ce06425f7ebd23645a6181a33
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-03-26 22:59:13 -0300

    Printing Message Timestamps

--- Node.js - The Complete Developer Course/node-chat-app/playground/time.js
+++ Node.js - The Complete Developer Course/node-chat-app/playground/time.js
@@ -1,7 +1,13 @@
 const moment = require('moment');
 
-const date = moment();
+/* const date = moment();
 date.add(1, 'years').subtract(9, 'months');
 console.log(date.format('MMM Do, YYYY'));
+*/
+const someTimestamp = moment().valueOf();
+console.log(someTimestamp);
 
-console.log(date.format('h:mm'));
+
+const createdAt = 1234;
+const date = moment(createdAt);
+console.log(date.format('h:mm a'));
