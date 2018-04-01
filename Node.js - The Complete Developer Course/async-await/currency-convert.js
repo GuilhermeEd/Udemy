a9f534169d6c54524e8ef58ebd8d43a99fd846cb
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-04-01 20:20:15 -0300

    Handling Errors and Awaiting Async Function

--- Node.js - The Complete Developer Course/async-await/currency-convert.js
+++ Node.js - The Complete Developer Course/async-await/currency-convert.js
@@ -1,15 +1,27 @@
 const axios = require('axios');
 
-const getExchangeRate = (from, to) => {
-  return axios.get(`http://api.fixer.io/latest?base=${from}`).then((response) => {
-    return response.data.rates[to];
-  });
+const getExchangeRate = async (from, to) => {
+  try {
+    const response = await axios.get(`http://api.fixer.io/latest?base=${from}`);
+    const rate = response.data.rates[to];
+
+    if (rate) {
+      return rate;
+    } else {
+      throw new Error();
+    }
+  } catch (err) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}.`);
+  }
 };
 
-const getCountries = (currencyCode) => {
-  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+const getCountries = async (currencyCode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
     return response.data.map( country => country.name);
-  });
+  } catch (err) {
+    throw new Error(`Unable to get countries that user ${currencyCode}`);
+  }
 };
 
 const convertCurrency = (from, to, amount) => {
@@ -46,4 +58,6 @@ convertCurrency('CAD', 'USD', 100).then( status => {
 
 convertCurrencyAlt('CAD', 'USD', 100).then( status => {
   console.log(status);
+}).catch( err => {
+  console.log(err.message);
 });
