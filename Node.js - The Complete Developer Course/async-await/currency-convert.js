d69d4f16b3a2006abda16d2e3ad4d9c8ad11ec24
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-04-01 20:13:11 -0300

    A Real-World Example

---
+++ Node.js - The Complete Developer Course/async-await/currency-convert.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+
+const getExchangeRate = (from, to) => {
+  return axios.get(`http://api.fixer.io/latest?base=${from}`).then((response) => {
+    return response.data.rates[to];
+  });
+};
+
+const getCountries = (currencyCode) => {
+  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+    return response.data.map( country => country.name);
+  });
+};
+
+const convertCurrency = (from, to, amount) => {
+  let countries;
+  return getCountries(to).then( tempCountries => {
+    countries = tempCountries;
+    return getExchangeRate(from, to);
+  }).then( rate => {
+    const exchangedAmount = amount * rate;
+
+    return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
+  });
+};
+
+const convertCurrencyAlt = async (from, to, amount) => {
+  const countries = await getCountries(to);
+  const rate = await getExchangeRate(from, to);
+  const exchangedAmount = amount * rate;
+
+    return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
+};
+
+getExchangeRate('USD', 'CAD').then(rate => {
+  console.log(rate);
+});
+
+getCountries('CAD').then(countries => {
+  console.log(countries);
+});
+
+convertCurrency('CAD', 'USD', 100).then( status => {
+  console.log(status);
+});
+
+convertCurrencyAlt('CAD', 'USD', 100).then( status => {
+  console.log(status);
+});
