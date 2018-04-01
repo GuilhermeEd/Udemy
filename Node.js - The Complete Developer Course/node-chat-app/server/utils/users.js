a71ef4d518597f0c40fc32ea79a270078415f419
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-04-01 19:34:05 -0300

    Wiring Up User List

--- Node.js - The Complete Developer Course/node-chat-app/server/utils/users.js
+++ Node.js - The Complete Developer Course/node-chat-app/server/utils/users.js
@@ -18,32 +18,32 @@
 var me = new Person('Guilherme', 25);
  */
 
- class Users {
-   constructor () {
-     this.users = [];
-   }
-   addUser (id, name, room) {
-    var user = {id, name, room};
+class Users {
+  constructor() {
+    this.users = [];
+  }
+  addUser(id, name, room) {
+    var user = { id, name, room };
     this.users.push(user);
     return user;
-   }
-   removeUser (id) {
+  }
+  removeUser(id) {
     var user = this.getUser(id);
 
     if (user) {
-      this.users = this.users.filter( user => user.id !== id);
+      this.users = this.users.filter(user => user.id !== id);
     }
 
     return user;
-   }
-   getUser (id) {
-    return this.users.filter( user => user.id === id )[0];
-   }
-   getUserList (room) {
-     var users = this.users.filter ( user => user.room === room );
-     var namesArray = users.map ( user => user.name );
-     return namesArray;
-   }
- }
+  }
+  getUser(id) {
+    return this.users.filter(user => user.id === id)[0];
+  }
+  getUserList(room) {
+    var users = this.users.filter(user => user.room === room);
+    var namesArray = users.map(user => user.name);
+    return namesArray;
+  }
+}
 
- module.exports = { Users };
+module.exports = { Users };
